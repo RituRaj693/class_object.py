a105abaa7c55890385b58e6056bd1ab76908e5bd
RituRaj693 <45709450+RituRaj693@users.noreply.github.com>
2019-05-28 00:38:12 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,9 @@
+class laptop:
+    def __init__(self,brand,model,price):
+        self.brand=brand
+        self.model=model
+        self.price=price
+p1=laptop('apple','macbook',500000)
+print(p1.brand)
+print(p1.model)
+print(p1.price)
